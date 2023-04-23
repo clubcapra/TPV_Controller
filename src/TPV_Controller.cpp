9474b9a47cc485552268baf24eecb60007dbb8ab
Benoit Malenfant <benmalenfant@gmail.com>
2023-04-23 17:24:40 -0400

    FB servo code cleanup - now uses a struct

--- src/TPV_Controller.cpp
+++ src/TPV_Controller.cpp
@@ -5,36 +5,35 @@
 #include <std_msgs/Float64.h>
 #include <Servo.h>
 
-#define KP -0.2
+#define SERVO1_CMD_PIN 5
+#define SERVO1_FB_PIN  2
+#define SERVO2_CMD_PIN 6
+#define SERVO2_FB_PIN  3
 
-#define G_X 0.7
-#define G_Y 0.7
-
-//Command to send
-float cmd1 = 0;
-float cmd2 = 0;
-
-//Live position of servos
-float pos1 = 0;
-float pos2 = 0;
-
-ros::NodeHandle nh;
-
-Servo servo1;
-Servo servo2;
-
-
-long lasttime;
+struct fb_servo { //Feedback servo struct
+  int turns = 0;
+  unsigned long rise=0, fall=0, tLow=0, tHigh=0, last_reading = 0, last_command = 0;
+  float thetaPre = 0;
+  float rate = 0;  //velocity command
+  float cmd = 0;
+  float pos = 0;
+  float rate_gain = 0.7;
+  float kp_gain = -0.2;
+  int valid = 0;
+  Servo servo_handle;
+};
 
 void interupt1();
 void interupt2();
+void messageServo1(const std_msgs::Float64& toggle_msg);
+void messageServo2(const std_msgs::Float64& toggle_msg);
+void fb_servo_update(fb_servo fb_servo_handle);
+void fb_servo_calc_angle(fb_servo servo);
 
-void messageServo1(const std_msgs::Float64& toggle_msg){
-  cmd1 = toggle_msg.data;
-}
-void messageServo2(const std_msgs::Float64& toggle_msg){
-  cmd2 = toggle_msg.data;
-}
+fb_servo servo1;
+fb_servo servo2;
+
+ros::NodeHandle nh;
 
 std_msgs::Float64 tpv_pos_x;
 std_msgs::Float64 tpv_pos_y;
@@ -44,12 +43,11 @@ ros::Subscriber<std_msgs::Float64> sub2("tpv_y", &messageServo2);
 ros::Publisher pub1("tpv_pos_x", &tpv_pos_x);
 ros::Publisher pub2("tpv_pos_y", &tpv_pos_y);
 
-float angle1, angle2;
-
 void setup()
 {
-  servo1.attach(5);
-  servo2.attach(6);
+  servo1.servo_handle.attach(SERVO1_CMD_PIN);
+  servo2.servo_handle.attach(SERVO2_CMD_PIN);
+
   nh.getHardware()->setBaud(57600);
   nh.initNode();
   nh.subscribe(sub1);
@@ -57,124 +55,121 @@ void setup()
   nh.advertise(pub1);
   nh.advertise(pub2);
 
-  attachInterrupt(digitalPinToInterrupt(2), interupt1, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(3), interupt2, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(SERVO1_FB_PIN), interupt1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(SERVO2_FB_PIN), interupt2, CHANGE);
+
+  delay(500);
+
+  fb_servo_calc_angle(servo1);
+  fb_servo_calc_angle(servo2);
+
+  if(servo1.valid){
+    servo1.cmd = servo1.pos;
+  }
+  if(servo2.valid){
+    servo2.cmd = servo1.pos;
+  }
 }
-
-
-
-int unitsFC = 360;                          // Units in a full circle
-int dutyScale = 1000;                       // Scale duty cycle to 1/1000ths
-int dcMin = 31;                            // Minimum duty cycle
-int dcMax = 971;                            // Maximum duty cycle
-int q2min = unitsFC/4;                      // For checking if in 1st quadrant
-int q3max = q2min * 3;                      // For checking if in 4th quadrant
-
-int turns1 = 0;
-unsigned long rise1, fall1, tLow1, tHigh1;
-unsigned long rise2, fall2, tLow2, tHigh2;
-float thetaPre1, thetaPre2;
-int turns2 = 0;
 
 void loop()
 {
-  if(1){
-    int tCycle = tHigh1 + tLow1;
-    if((tCycle > 1000) && (tCycle < 1200)){
-      float dc = (dutyScale * tHigh1) / (float)tCycle;
-      float theta = (unitsFC - 1) - ((dc - dcMin) * unitsFC) / (dcMax - dcMin + 1);
+    fb_servo_update(servo1);
+    fb_servo_update(servo2);
     
-      
-      if(theta < 0.0)
-          theta = 0.0;
-      else if(theta > (unitsFC - 1.0))
-          theta = unitsFC - 1.0;
-    
-      if((theta < q2min) && (thetaPre1 > q3max))
-          turns1++;
-      else if((thetaPre1 < q2min) && (theta > q3max))
-          turns1--;
-    
-      if(turns1 >= 0)
-          angle1 = (turns1 * unitsFC) + theta;
-      else if(turns1 < 0)
-          angle1 = ((turns1 + 1) * unitsFC) - (unitsFC - theta);
-    
-      thetaPre1 = theta;
-    }
-  }
-
-//----------------------------------------------------------------
-  if(1){
-    int tCycle = tHigh2 + tLow2;
-    if((tCycle > 1000) && (tCycle < 1200)){
-      float dc = (dutyScale * tHigh2) / (float)tCycle;
-      float theta = (unitsFC - 1) - ((dc - dcMin) * unitsFC) / (dcMax - dcMin + 1);
-    
-      
-      if(theta < 0.0)
-          theta = 0.0;
-      else if(theta > (unitsFC - 1.0))
-          theta = unitsFC - 1.0;
-    
-      if((theta < q2min) && (thetaPre2 > q3max))
-          turns2++;
-      else if((thetaPre2 < q2min) && (theta > q3max))
-          turns2--;
-    
-      if(turns1 >= 0)
-          angle2 = (turns2 * unitsFC) + theta;
-      else if(turns1 < 0)
-          angle2 = ((turns2 + 1) * unitsFC) - (unitsFC - theta);
-    
-      thetaPre2 = theta;
-    }
-  }
-//-------------------------------------------------------------------
-
-
-  if(millis() > 200){
-    pos1 = pos1 + G_X*cmd1*(millis() - lasttime);
-    pos2 = pos2 + G_Y*cmd2*(millis() - lasttime);
-  
-    servo1.write(constrain(((angle1-pos1)*KP) + 90, 0 , 180));
-    servo2.write(constrain(((angle2-pos2)*KP) + 90, 0 , 180));
-    
-    tpv_pos_x.data = angle1;
+    tpv_pos_x.data = servo2.pos;
     pub1.publish(&tpv_pos_x);
   
-    tpv_pos_y.data = angle2;
+    tpv_pos_y.data = servo1.pos;
     pub1.publish(&tpv_pos_y);
-    
-    lasttime = millis();
+
     nh.spinOnce();
     delay(1);
-  }
-  else{
-    pos1 = angle1;
-    pos2 = angle2;
-  }
 }
 
 
-void interupt1(){
-    if(digitalRead(2)) {
-      rise1 = micros();
-      tLow1 = rise1 - fall1;
+void messageServo1(const std_msgs::Float64& toggle_msg){
+  servo1.rate = toggle_msg.data;
+}
+void messageServo2(const std_msgs::Float64& toggle_msg){
+  servo2.rate = toggle_msg.data;
+}
+
+void fb_servo_update(fb_servo fb_servo_handle){
+  fb_servo_calc_angle(fb_servo_handle);
+  if(fb_servo_handle.valid){
+    fb_servo_handle.cmd = fb_servo_handle.pos + fb_servo_handle.rate_gain*fb_servo_handle.rate*(millis() - fb_servo_handle.last_command);
+    fb_servo_handle.servo_handle.write(constrain(((fb_servo_handle.cmd-fb_servo_handle.pos)*fb_servo_handle.kp_gain) + 90, 0 , 180));
+  }
+  else{
+    fb_servo_handle.servo_handle.write(90);
+  }
+}
+
+void fb_servo_calc_angle(fb_servo fb_servo_handle){
+  int unitsFC = 360;                          // Units in a full circle
+  int dutyScale = 1000;                       // Scale duty cycle to 1/1000ths
+  int dcMin = 31;                            // Minimum duty cycle
+  int dcMax = 971;                            // Maximum duty cycle
+  int q2min = unitsFC/4;                      // For checking if in 1st quadrant
+  int q3max = q2min * 3;                      // For checking if in 4th quadrant
+
+  int tCycle = fb_servo_handle.tHigh + fb_servo_handle.tLow;
+  if(micros() - servo1.last_reading < 1200){
+    if((tCycle > 1000) && (tCycle < 1200)){
+      fb_servo_handle.valid=1;
+
+      float dc = (dutyScale * fb_servo_handle.tHigh) / (float)tCycle;
+      float theta = (unitsFC - 1) - ((dc - dcMin) * unitsFC) / (dcMax - dcMin + 1);
+      
+      if(theta < 0.0)
+          theta = 0.0;
+      else if(theta > (unitsFC - 1.0))
+          theta = unitsFC - 1.0;
+    
+      if((theta < q2min) && (fb_servo_handle.thetaPre > q3max))
+          fb_servo_handle.turns++;
+      else if((fb_servo_handle.thetaPre < q2min) && (theta > q3max))
+          fb_servo_handle.turns--;
+    
+      if(fb_servo_handle.turns >= 0)
+          fb_servo_handle.pos = (fb_servo_handle.turns * unitsFC) + theta;
+      else if(fb_servo_handle.turns < 0)
+          fb_servo_handle.pos = ((fb_servo_handle.turns + 1) * unitsFC) - (unitsFC - theta);
+    
+      fb_servo_handle.thetaPre = theta;
     }
     else{
-      fall1 = micros();
-      tHigh1 = fall1 - rise1;
+      fb_servo_handle.valid=0;
+      fb_servo_handle.last_command=micros();
+    }
+  }
+  else{
+    fb_servo_handle.valid=0;
+    fb_servo_handle.last_command=micros();
+  }
+
+}
+
+void interupt1(){
+    if(!digitalRead(SERVO1_FB_PIN)) {
+      servo1.rise = micros();
+      servo1.tLow = servo1.rise - servo1.fall;
+      servo1.last_reading = micros();
+    }
+    else{
+      servo1.fall = micros();
+      servo1.tHigh = servo1.fall - servo1.rise;
     }
 }
 
 void interupt2(){
-  if(digitalRead(3)) {
-    rise2 = micros();
-    tLow2 = rise2 - fall2;
+  if(!digitalRead(SERVO2_FB_PIN)) {
+    servo1.rise = micros();
+    servo1.tLow = servo1.rise - servo1.fall;
+    servo1.last_reading = micros();
   }
   else{
-    fall2 = micros();
-    tHigh2 = fall2 - rise2;
+    servo1.fall = micros();
+    servo1.tHigh = servo1.fall - servo1.rise;
   }
 }
