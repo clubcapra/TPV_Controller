84447ee51d5924488831cf90d11c32fdfc1bb8d5
Samuel Lachance <leuchak@slachance.ca>
2023-06-12 13:04:34 +0200

    Format TPV_Controller.cpp

--- src/TPV_Controller.cpp
+++ src/TPV_Controller.cpp
@@ -5,33 +5,33 @@
 #include <std_msgs/Float64.h>
 #include <Servo.h>
 
-#define UPDATE_RATE 15.0                //UPDATE RATE IN HZ
-#define UPDATE_PERIOD 1.0/UPDATE_RATE   //UPDATE RATE IN S
+#define UPDATE_RATE 15.0                // UPDATE RATE IN HZ
+#define UPDATE_PERIOD 1.0 / UPDATE_RATE // UPDATE RATE IN S
 
 #define SERVO1_CMD_PIN 5
 #define SERVO1_FB_PIN  2
 #define SERVO2_CMD_PIN 6
 #define SERVO2_FB_PIN  3
 
-#define VBUS1_PIN  A0
-#define VBUS2_PIN  A1
+#define VBUS1_PIN A0
+#define VBUS2_PIN A1
 
-#define DOP1_PIN  A2
-#define DOP2_PIN  A3
-#define DOP3_PIN  A4
-#define DOP4_PIN  A5
+#define DOP1_PIN A2
+#define DOP2_PIN A3
+#define DOP3_PIN A4
+#define DOP4_PIN A5
 
 #define X_OFFSET -275
 #define Y_OFFSET -35
 
-
 #define VBUS_RATIO 6.23
 
-struct fb_servo { //Feedback servo struct
+struct fb_servo
+{ // Feedback servo struct
   int turns = 0;
-  unsigned long rise=0, fall=0, tLow=0, tHigh=0, last_reading = 0, last_command = 0;
+  unsigned long rise = 0, fall = 0, tLow = 0, tHigh = 0, last_reading = 0, last_command = 0;
   float thetaPre = 0;
-  float rate = 0;  //velocity command
+  float rate = 0; // velocity command
   float cmd = 0;
   float pos = 0;
   float rate_gain = 4;
@@ -43,15 +43,15 @@ struct fb_servo { //Feedback servo struct
   Servo servo_handle;
 };
 
-void messageServo1(const std_msgs::Float64& toggle_msg);
-void messageServo2(const std_msgs::Float64& toggle_msg);
-void messageDop1(const std_msgs::UInt16& toggle_msg);
-void messageDop2(const std_msgs::UInt16& toggle_msg);
-void messageDop3(const std_msgs::UInt16& toggle_msg);
-void messageDop4(const std_msgs::UInt16& toggle_msg);
+void messageServo1(const std_msgs::Float64 &toggle_msg);
+void messageServo2(const std_msgs::Float64 &toggle_msg);
+void messageDop1(const std_msgs::UInt16 &toggle_msg);
+void messageDop2(const std_msgs::UInt16 &toggle_msg);
+void messageDop3(const std_msgs::UInt16 &toggle_msg);
+void messageDop4(const std_msgs::UInt16 &toggle_msg);
 
-void fb_servo_update(fb_servo* fb_servo_handle);
-void fb_servo_calc_angle(fb_servo* servo);
+void fb_servo_update(fb_servo *fb_servo_handle);
+void fb_servo_calc_angle(fb_servo *servo);
 void interupt1();
 void interupt2();
 
@@ -79,10 +79,10 @@ ros::Publisher pub4("vbus2", &vbus2);
 
 void setup()
 {
-  pinMode(DOP1_PIN,OUTPUT);
-  pinMode(DOP2_PIN,OUTPUT);
-  pinMode(DOP3_PIN,OUTPUT);
-  pinMode(DOP4_PIN,OUTPUT);
+  pinMode(DOP1_PIN, OUTPUT);
+  pinMode(DOP2_PIN, OUTPUT);
+  pinMode(DOP3_PIN, OUTPUT);
+  pinMode(DOP4_PIN, OUTPUT);
 
   servo1.servo_handle.attach(SERVO1_CMD_PIN);
   servo2.servo_handle.attach(SERVO2_CMD_PIN);
@@ -112,144 +112,194 @@ void setup()
   fb_servo_calc_angle(&servo1);
   fb_servo_calc_angle(&servo2);
 
-  if(servo1.valid){
+  if (servo1.valid)
+  {
     servo1.cmd = servo1.pos;
   }
-  if(servo2.valid){
+  if (servo2.valid)
+  {
     servo2.cmd = servo2.pos;
   }
 }
 
 void loop()
 {
-    unsigned long timestamp = micros();
-    fb_servo_update(&servo1);
-    fb_servo_update(&servo2);
-    
-    tpv_pos_x.data = servo1.pos - X_OFFSET;
-    pub1.publish(&tpv_pos_x);
-  
-    tpv_pos_y.data = servo2.pos - Y_OFFSET;
-    pub2.publish(&tpv_pos_y);
-    
-    Serial.println(servo1.tLow);
+  unsigned long timestamp = micros();
+  fb_servo_update(&servo1);
+  fb_servo_update(&servo2);
 
-    vbus1.data = analogRead(VBUS1_PIN)/1023.0 * 5 * VBUS_RATIO;
-    pub3.publish(&vbus1);
+  tpv_pos_x.data = servo1.pos - X_OFFSET;
+  pub1.publish(&tpv_pos_x);
 
-    vbus2.data = analogRead(VBUS2_PIN)/1023.0* 5 * VBUS_RATIO;
-    pub4.publish(&vbus2);
+  tpv_pos_y.data = servo2.pos - Y_OFFSET;
+  pub2.publish(&tpv_pos_y);
 
-    nh.spinOnce();
+  Serial.println(servo1.tLow);
 
+  vbus1.data = analogRead(VBUS1_PIN) / 1023.0 * 5 * VBUS_RATIO;
+  pub3.publish(&vbus1);
 
-    while(micros()-timestamp < UPDATE_PERIOD * 1000000.0){
-    }
+  vbus2.data = analogRead(VBUS2_PIN) / 1023.0 * 5 * VBUS_RATIO;
+  pub4.publish(&vbus2);
+
+  nh.spinOnce();
+
+  while (micros() - timestamp < UPDATE_PERIOD * 1000000.0)
+  {
+  }
 }
 
-void messageDop1(const std_msgs::UInt16& toggle_msg){
+void messageDop1(const std_msgs::UInt16 &toggle_msg)
+{
   digitalWrite(DOP1_PIN, toggle_msg.data);
 }
 
-void messageDop2(const std_msgs::UInt16& toggle_msg){
+void messageDop2(const std_msgs::UInt16 &toggle_msg)
+{
   digitalWrite(DOP2_PIN, toggle_msg.data);
 }
 
-void messageDop3(const std_msgs::UInt16& toggle_msg){
+void messageDop3(const std_msgs::UInt16 &toggle_msg)
+{
   digitalWrite(DOP3_PIN, toggle_msg.data);
 }
 
-void messageDop4(const std_msgs::UInt16& toggle_msg){
+void messageDop4(const std_msgs::UInt16 &toggle_msg)
+{
   digitalWrite(DOP4_PIN, toggle_msg.data);
 }
 
-void messageServo1(const std_msgs::Float64& toggle_msg){
+void messageServo1(const std_msgs::Float64 &toggle_msg)
+{
   servo1.rate = toggle_msg.data;
 }
 
-void messageServo2(const std_msgs::Float64& toggle_msg){
+void messageServo2(const std_msgs::Float64 &toggle_msg)
+{
   servo2.rate = toggle_msg.data;
 }
 
-void fb_servo_update(fb_servo* fb_servo_handle){
+/// @brief Servo feedback control
+/// @param servo Pointer to the servo object
+void fb_servo_update(fb_servo *fb_servo_handle)
+{
+  // Calculate the servo angle based on the input signal
   fb_servo_calc_angle(fb_servo_handle);
-  if(fb_servo_handle->valid){
-    fb_servo_handle->cmd = fb_servo_handle->cmd + fb_servo_handle->rate_gain*fb_servo_handle->rate;
-    float error = (fb_servo_handle->cmd-fb_servo_handle->pos);
-    fb_servo_handle->i = constrain(fb_servo_handle->i + error, -fb_servo_handle->i_max , fb_servo_handle->i_max);
-    float servo_command = constrain((error*fb_servo_handle->kp_gain + fb_servo_handle->i*fb_servo_handle->ki_gain) + 90, 0 , 180);
-    fb_servo_handle->servo_handle.write(fb_servo_handle->rate * fb_servo_handle->kp_gain + 90);
-  }
-  else{
+
+  if (fb_servo_handle->valid == 0)
+  {
+    // Set the servo position to the neutral position
     fb_servo_handle->servo_handle.write(90);
   }
+  else
+  {
+    // Calculate the new command based on the rate and rate gain
+    fb_servo_handle->cmd = fb_servo_handle->cmd + fb_servo_handle->rate_gain * fb_servo_handle->rate;
+
+    // Calculate the error between the command and the current position
+    float error = (fb_servo_handle->cmd - fb_servo_handle->pos);
+
+    // Update the integral term of the controller and constrain it to the maximum value
+    fb_servo_handle->i = constrain(fb_servo_handle->i + error, -fb_servo_handle->i_max, fb_servo_handle->i_max);
+
+    // Calculate the servo command based on the error and the gains, and constrain it to the valid range
+    float servo_command = constrain((error * fb_servo_handle->kp_gain + fb_servo_handle->i * fb_servo_handle->ki_gain) + 90, 0, 180);
+
+    // Write the servo command to the servo motor
+    fb_servo_handle->servo_handle.write(servo_command);
+  }
 }
 
-void fb_servo_calc_angle(fb_servo* fb_servo_handle){
-  int unitsFC = 360;                          // Units in a full circle
-  int dutyScale = 1000;                       // Scale duty cycle to 1/1000ths
-  int dcMin = 31;                            // Minimum duty cycle
-  int dcMax = 971;                            // Maximum duty cycle
-  int q2min = unitsFC/4;                      // For checking if in 1st quadrant
-  int q3max = q2min * 3;                      // For checking if in 4th quadrant
+/// @brief Calculate the angle of the servo
+/// @param fb_servo_handle Pointer to the servo object
+void fb_servo_calc_angle(fb_servo *fb_servo_handle)
+{
+  int unitsFC = 360;       // Units in a full circle
+  int dutyScale = 1000;    // Scale duty cycle to 1/1000ths
+  int dcMin = 31;          // Minimum duty cycle
+  int dcMax = 971;         // Maximum duty cycle
+  int q2min = unitsFC / 4; // For checking if in 1st quadrant
+  int q3max = q2min * 3;   // For checking if in 4th quadrant
 
   int tCycle = fb_servo_handle->tHigh + fb_servo_handle->tLow;
-  if((micros() - fb_servo_handle->last_reading) < 1200){
-    if((tCycle > 1000) && (tCycle < 1200)){
-      fb_servo_handle->valid=1;
+  if ((micros() - fb_servo_handle->last_reading) >= 1200)
+  {
+    // Signal has not been read recently, update servo state
+    fb_servo_handle->valid = 0;
+    fb_servo_handle->last_command = micros();
+  }
+  else
+  {
+    if (tCycle <= 1000 && tCycle >= 1200)
+    {
+      // Signal is not valid, update servo state
+      fb_servo_handle->valid = 0;
+      fb_servo_handle->last_command = micros();
+    }
+    else
+    {
+      fb_servo_handle->valid = 1;
 
+      // Calculate duty cycle and angle
       float dc = (dutyScale * fb_servo_handle->tHigh) / (float)tCycle;
       float theta = (unitsFC - 1) - ((dc - dcMin) * unitsFC) / (dcMax - dcMin + 1);
-      
-      if(theta < 0.0)
-          theta = 0.0;
-      else if(theta > (unitsFC - 1.0))
-          theta = unitsFC - 1.0;
-    
-      if((theta < q2min) && (fb_servo_handle->thetaPre > q3max))
-          fb_servo_handle->turns++;
-      else if((fb_servo_handle->thetaPre < q2min) && (theta > q3max))
-          fb_servo_handle->turns--;
-    
-      if(fb_servo_handle->turns >= 0)
-          fb_servo_handle->pos = (fb_servo_handle->turns * unitsFC) + theta;
-      else if(fb_servo_handle->turns < 0)
-          fb_servo_handle->pos = ((fb_servo_handle->turns + 1) * unitsFC) - (unitsFC - theta);
-    
+
+      // Clamp angle to valid range
+      if (theta < 0.0)
+        theta = 0.0;
+      else if (theta > (unitsFC - 1.0))
+        theta = unitsFC - 1.0;
+
+      // Check for servo turns and update position
+      if ((theta < q2min) && (fb_servo_handle->thetaPre > q3max))
+        fb_servo_handle->turns++;
+      else if ((fb_servo_handle->thetaPre < q2min) && (theta > q3max))
+        fb_servo_handle->turns--;
+
+      if (fb_servo_handle->turns >= 0)
+        fb_servo_handle->pos = (fb_servo_handle->turns * unitsFC) + theta;
+      else if (fb_servo_handle->turns < 0)
+        fb_servo_handle->pos = ((fb_servo_handle->turns + 1) * unitsFC) - (unitsFC - theta);
+
       fb_servo_handle->thetaPre = theta;
     }
-    else{
-      fb_servo_handle->valid=0;
-      fb_servo_handle->last_command=micros();
-    }
   }
-  else{
-    fb_servo_handle->valid=0;
-    fb_servo_handle->last_command=micros();
-  }
-
 }
 
-void interupt1(){
-    if(!digitalRead(SERVO1_FB_PIN)) {
-      servo1.rise = micros();
-      servo1.tLow = servo1.rise - servo1.fall;
-      servo1.last_reading = micros();
-    }
-    else{
-      servo1.fall = micros();
-      servo1.tHigh = servo1.fall - servo1.rise;
-    }
+/// @brief Interrupt service routine for reading the feedback signal of servo 1
+void interupt1()
+{
+  // If the feedback signal is high
+  if (digitalRead(SERVO1_FB_PIN))
+  {
+    // Record the time of the falling edge of the feedback signal
+    servo1.fall = micros();
+    servo1.tHigh = servo1.fall - servo1.rise;
+  }
+  else
+  {
+    // Record the time of the rising edge of the feedback signal
+    servo1.rise = micros();
+    servo1.tLow = servo1.rise - servo1.fall;
+    servo1.last_reading = micros();
+  }
 }
 
-void interupt2(){
-  if(!digitalRead(SERVO2_FB_PIN)) {
+/// @brief Interrupt service routine for reading the feedback signal of servo 2
+void interupt2()
+{
+  // If the feedback signal is high
+  if (digitalRead(SERVO2_FB_PIN))
+  {
+    // Record the time of the falling edge of the feedback signal
+    servo2.fall = micros();
+    servo2.tHigh = servo2.fall - servo2.rise;
+  }
+  else
+  {
+    // Record the time of the rising edge of the feedback signal
     servo2.rise = micros();
     servo2.tLow = servo2.rise - servo2.fall;
     servo2.last_reading = micros();
-  }
-  else{
-    servo2.fall = micros();
-    servo2.tHigh = servo2.fall - servo2.rise;
   }
 }
